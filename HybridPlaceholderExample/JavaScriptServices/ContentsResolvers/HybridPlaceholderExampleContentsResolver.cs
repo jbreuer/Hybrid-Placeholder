c8af3df0ead76f353e01cbcfb6b2d11435117ffd
Jeroen Breuer <jbreuer@gmail.com>
2022-02-25 11:50:17 +0100

    Added solution.

---
+++ HybridPlaceholderExample/JavaScriptServices/ContentsResolvers/HybridPlaceholderExampleContentsResolver.cs
@@ -0,0 +1,30 @@
+﻿namespace HybridPlaceholderExample.JavaScriptServices.ContentsResolvers
+{
+    using System;
+    using System.Collections.Specialized;
+    using Sitecore.LayoutService.Configuration;
+    using Sitecore.LayoutService.ItemRendering.ContentsResolvers;
+    using Sitecore.Mvc.Presentation;
+
+    public class HybridPlaceholderExampleContentsResolver : IRenderingContentsResolver
+    {
+        public object ResolveContents(Rendering rendering, IRenderingConfiguration renderingConfig)
+        {
+            var datasource = !string.IsNullOrEmpty(rendering.DataSource)
+                ? rendering.RenderingItem?.Database.GetItem(rendering.DataSource)
+                : null;
+            
+            return new
+            {
+                name = datasource?.Name,
+                date = DateTime.Now,
+                hello = "world"
+            };
+        }
+
+        public bool IncludeServerUrlInMediaUrls { get; set; }
+        public bool UseContextItem { get; set; }
+        public string ItemSelectorQuery { get; set; }
+        public NameValueCollection Parameters { get; set; }
+    }
+}
