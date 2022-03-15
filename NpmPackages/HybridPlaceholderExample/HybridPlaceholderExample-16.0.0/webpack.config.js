2ad3605b1a9f4834975e87116fc78776f7dbae23
Jeroen Breuer <jbreuer@gmail.com>
2022-03-15 11:53:17 +0100

    Added npm packages for Hybrid Placeholder Example. Versions match Sitecore JSS versions.

--- NpmPackages/HybridPlaceholderExample/HybridPlaceholderExample-16.0.0/webpack.config.js
+++ NpmPackages/HybridPlaceholderExample/HybridPlaceholderExample-16.0.0/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
-  entry: './HybridPlaceholderExample.jsx',
+  entry: '../../../HybridPlaceholderExample/UserInterface/HybridPlaceholderExample.jsx',
   output: {
     path: path.resolve('dist'),
     filename: 'index.js',
